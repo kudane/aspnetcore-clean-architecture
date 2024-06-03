813f85dffdf0bc8536fcea1a5c0963519dc7efd9
Kraisorn.R <kraisorn204@gmail.com>
2024-06-03 21:51:19 +0700

    Update Program.cs

--- DesignBusinessCore/DesignBusinessCore/Program.cs
+++ DesignBusinessCore/DesignBusinessCore/Program.cs
@@ -4,6 +4,6 @@ using DesignBusinessCore.Implementation;
 // on web service (nice syntax for encapsulation concrete class to interface base)
 //services.AddScope<IPingHandler, PingHandler>();
 
-// manual used, create by interface type
+// manual used, new by interface type
 IPingHandler ping = new PingHandler();
 var success = ping.Handle(new PingCommand() { Message = "ping all green" });
